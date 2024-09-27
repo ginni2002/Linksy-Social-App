12f88276e359ce67f1909b6184a77e10fd3a6bb8
Giridhari <grsb2002@gmail.com>
2024-09-27 21:28:27 +0530

    login created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../model/userModel.js";
+import genTokenAndSetCookie from "../utils/helpers/genTokenAndSetCookie.js";
 
 const signupUser = async (req, res) => {
   try {
@@ -21,6 +22,8 @@ const signupUser = async (req, res) => {
     });
     await newUser.save();
     if (newUser) {
+      genTokenAndSetCookie(newUser._id, res);
+
       res.status(201).json({
         _id: newUser._id,
         name: newUser.name,
@@ -36,4 +39,38 @@ const signupUser = async (req, res) => {
   }
 };
 
-export { signupUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+    genTokenAndSetCookie(user._id, res);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in loginUser: ", err.message);
+  }
+};
+
+const logoutUser = (req, res) => {
+  try {
+    res.cookie("jwtName1", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in logoutUser: ", err.message);
+  }
+};
+export { signupUser, loginUser, logoutUser };
