183767e92473cc6a6976d42928402ed62dc5ad8c
Giridhari <grsb2002@gmail.com>
2024-09-28 17:25:02 +0530

    created apis for posts

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,24 @@
 import express from "express";
 
-import { createPost } from "../controllers/postController.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+  getUserPosts,
+} from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoutes.js";
 
 const router = express.Router();
 
+router.get("/feed", protectRoute, getFeedPosts);
+router.get("/:id", getPost);
+router.get("/user/:username", getUserPosts);
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:id", protectRoute, replyToPost);
+
 export default router;
