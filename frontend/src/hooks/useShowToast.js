91714de6400bd095827fe787729495c89bfbff79
Giridhari <grsb2002@gmail.com>
2024-09-29 04:17:47 +0530

    created custom hooks

---
+++ frontend/src/hooks/useShowToast.js
@@ -0,0 +1,18 @@
+import { useToast } from "@chakra-ui/react";
+
+// custom hooks
+const useShowToast = () => {
+  const toast = useToast();
+  const showToast = (title, description, status) => {
+    toast({
+      title,
+      description,
+      status,
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+  return showToast;
+};
+
+export default useShowToast;
