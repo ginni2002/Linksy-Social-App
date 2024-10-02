391373e94e891c4e1ba28bae97a0a62644a19b01
Giridhari <grsb2002@gmail.com>
2024-10-02 23:06:51 +0530

    added reply functionality

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,8 +1,22 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Input,
+  Modal,
+  ModalCloseButton,
+  ModalOverlay,
+  Text,
+  FormControl,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Button,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
 import useShowToast from "../hooks/useShowToast";
+
 const Actions = ({ post: post_ }) => {
   const user = useRecoilValue(userAtom);
   const [liked, setLiked] = useState(post_.likes.includes(user?._id));
@@ -10,6 +24,9 @@ const Actions = ({ post: post_ }) => {
   const showToast = useShowToast();
   const [post, setPost] = useState(post_);
   const [isLiking, setIsLiking] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+  const [reply, setReply] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const handleLikeAndUnlike = async () => {
     if (!user) {
       return showToast(
@@ -27,7 +44,7 @@ const Actions = ({ post: post_ }) => {
           "Content-Type": "application/json",
         },
       });
-      const data = res.json();
+      const data = await res.json();
       if (data.error) {
         return showToast("Error", data.error, "error");
       }
@@ -44,6 +61,38 @@ const Actions = ({ post: post_ }) => {
       showToast("Error", error.message, "error");
     } finally {
       setIsLiking(false);
+    }
+  };
+  const handleReply = async () => {
+    if (!user)
+      return showToast(
+        "Error",
+        "You must be logged in to reply to a post",
+        "error"
+      );
+    if (isReplying) return;
+    setIsReplying(true);
+    try {
+      const res = await fetch("/api/posts/reply" + post._id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: reply }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      setPost({ ...post, replies: [...post.replies, data.reply] });
+      showToast("Success", "Reply posted successfully", "success");
+      onClose();
+      setReply("");
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsReplying(false);
     }
   };
   return (
@@ -74,6 +123,7 @@ const Actions = ({ post: post_ }) => {
           role="img"
           viewBox="0 0 24 24"
           width="20"
+          onClick={onOpen}
         >
           <title>Comment</title>
           <path
@@ -96,6 +146,35 @@ const Actions = ({ post: post_ }) => {
           {post.likes.length} likes
         </Text>
       </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Input
+                placeholder="Reply goes here..."
+                value={reply}
+                onChange={(e) => setReply(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              size={"sm"}
+              mr={3}
+              isLoading={isReplying}
+              onClick={handleReply}
+            >
+              Reply
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
