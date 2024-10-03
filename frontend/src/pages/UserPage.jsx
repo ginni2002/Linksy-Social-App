b3432876bb04bf0841631b92d3f0b0db3aecbbbc
Giridhari <grsb2002@gmail.com>
2024-10-03 16:19:46 +0530

    modified userpage

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,32 +1,35 @@
 import { useEffect, useState } from "react";
 import UserHeader from "../components/UserHeader";
-import UserPost from "../components/UserPost";
+
 import useShowToast from "../hooks/useShowToast";
 import { useParams } from "react-router-dom";
 import { Flex, Spinner } from "@chakra-ui/react";
+import Post from "../components/Post";
+import useGetUserProfile from "../hooks/useGetUserProfile";
 
 const UserPage = () => {
-  const [user, setUser] = useState(null);
+  const { user, loading } = useGetUserProfile();
   const { username } = useParams();
   const showToast = useShowToast();
-  const [loading, setLoading] = useState(true);
+
+  const [posts, setPosts] = useState([]);
+  const [fetchingPosts, setFetchingPosts] = useState(true);
   useEffect(() => {
-    const getUser = async () => {
+    const getPosts = async () => {
+      setFetchingPosts(true);
       try {
-        const res = await fetch(`/api/users/profile/${username}`);
+        const res = await fetch(`/api/posts/user/${username}`);
         const data = await res.json();
-        if (data.error) {
-          showToast("Error", data.error, "error");
-          return;
-        }
-        setUser(data);
+        setPosts(data);
       } catch (error) {
-        showToast("Error", error, "error");
+        showToast("Error", error.message, "error");
+        setPosts([]);
       } finally {
-        setLoading(false);
+        setFetchingPosts(false);
       }
     };
-    getUser();
+
+    getPosts();
   }, [username, showToast]);
   if (!user && loading) {
     return (
@@ -36,32 +39,24 @@ const UserPage = () => {
     );
   }
   if (!user && !loading) return <h1>User not found :(</h1>;
+
   return (
     <>
       <UserHeader user={user} />
-      <UserPost
-        likes={1200}
-        replies={481}
-        postImg="/post1.png"
-        postTitle="Let's talk about linklets"
-      />
-      <UserPost
-        likes={1120}
-        replies={41}
-        postImg="/post2.png"
-        postTitle="I can do better"
-      />
-      <UserPost
-        likes={3200}
-        replies={432}
-        postImg="/post3.png"
-        postTitle="Giridhari richer than him"
-      />
-      <UserPost
-        likes={890}
-        replies={367}
-        postTitle="This is my first linklet, cool"
-      />
+
+      {!fetchingPosts && posts.length === 0 && (
+        <h1>User has not posted yet! </h1>
+      )}
+
+      {fetchingPosts && (
+        <Flex justifyContent={"center"} my={12}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
     </>
   );
 };
