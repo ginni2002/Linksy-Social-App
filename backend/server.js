6968f6acbce0e88f4f5ddd5695ad5e03e41aa9d6
Giridhari <grsb2002@gmail.com>
2024-10-05 13:40:54 +0530

    added backend functionality for messages

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(cookieParser());
 //Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at https://localhost:${PORT}`)
